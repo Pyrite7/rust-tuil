9199edbd1cf29468430fc69aef2bc21b32ba0b19
Pyry Juvonen <pyryjuvonen@gmail.com>
2025-02-13 22:24:42 +0200

    new methods for Draw

--- src/draw_system/draw.rs
+++ src/draw_system/draw.rs
@@ -18,6 +18,15 @@ pub trait Draw {
     // Required methods
     fn get_cell(&self, pos: ScrPos) -> Option<StyledChar>;
 
+    
+    // Provided methods
+    fn is_opaque_at(&self, pos: ScrPos) -> bool {
+        self.get_cell(pos).is_some()
+    }
+
+    fn is_transparent_at(&self, pos: ScrPos) -> bool {
+        !self.is_opaque_at(pos)
+    }
 }
 
 
