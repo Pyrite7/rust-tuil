c1d5d83e589c6c86e4a6aff5327e311d43b1cdaf
Pyry Juvonen <pyryjuvonen@gmail.com>
2025-02-13 22:28:38 +0200

    simplified Redraw; SimpleRedraw should actually work now

--- src/redraw_system/redraw.rs
+++ src/redraw_system/redraw.rs
@@ -10,13 +10,7 @@ use super::redraw_buffer::RedrawBuffer;
 
 pub trait Redraw<Data>: Draw {
     // Required methods
-    fn update_data_and_redraw_positions(&mut self, update_data: Data) -> Vec<ScrPos>;
-
-    // Provided methods
-    fn update(&mut self, buffer: &mut RedrawBuffer, update_data: Data) {
-        let redraw_positions = self.update_data_and_redraw_positions(update_data);
-        buffer.add_redraw_positions(redraw_positions);
-    }
+    fn update(&mut self, buffer: &mut RedrawBuffer, update_data: Data) -> ();
 }
 
 
--- src/utility/simple_redraw.rs
+++ src/utility/simple_redraw.rs
@@ -9,14 +9,13 @@ pub trait SimpleRedraw: Draw + Clone {
 }
 
 impl<T: SimpleRedraw> Redraw<T> for T {
-    fn update_data_and_redraw_positions(&mut self, update_data: T) -> Vec<crate::ScrPos> {
+    fn update(&mut self, buffer: &mut crate::redraw_system::redraw_buffer::RedrawBuffer, update_data: T) -> () {
         let old = self.clone();
-        let old_positions = old.bounding_box().into_iter();
-        let new_positions = update_data.bounding_box().into_iter();
 
         *self = update_data;
 
-        old_positions.chain(new_positions).collect()
+        buffer.add_filtered_redraw_rect(old.bounding_box(), |pos| old.is_opaque_at(*pos));
+        buffer.add_filtered_redraw_rect(self.bounding_box(), |pos| self.is_opaque_at(*pos));
     }
 } 
 
